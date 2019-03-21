656c09f0d4702eef0ca3c8ef0a7d28c2906a179d
Tracy Cummings <TLCummings90@gmail.com>
2019-03-20 23:11:25 -0400

    messageService and driver working, may need to add more methods later on

--- src/main/java/com/revature/services/MessageService.java
+++ src/main/java/com/revature/services/MessageService.java
@@ -1,5 +1,81 @@
 package com.revature.services;
 
+import java.util.List;
+
+import javax.persistence.Query;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.revature.models.Message;
+
 public class MessageService {
 
+	public List<Message> getAll() {
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Message.class)
+				.buildSessionFactory();
+
+		Session session = factory.getCurrentSession();
+
+		session.beginTransaction();
+
+		Query query = session.createQuery("from Message");
+
+		List<Message> messages = query.getResultList();
+
+		factory.close();
+		return messages;
+
+	}
+
+	public Message getById(int id) {
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Message.class)
+				.buildSessionFactory();
+
+		Session session = factory.getCurrentSession();
+
+		session.beginTransaction();
+
+		Message message = session.get(Message.class, id);
+
+		factory.close();
+		return message;
+
+	}
+
+	public Message createMessage(int receiver, int sender, String content) {
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Message.class)
+				.buildSessionFactory();
+
+		Session session = factory.getCurrentSession();
+
+		session.beginTransaction();
+
+		Message message = new Message(sender, receiver, content);
+
+		session.save(message);
+		session.getTransaction().commit();
+
+		factory.close();
+		return message;
+
+	}
+
+	public void deleteMessage(int id) {
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Message.class)
+				.buildSessionFactory();
+
+		Session session = factory.getCurrentSession();
+
+		session.beginTransaction();
+
+		Query query = session.createQuery("delete from Message m where m.id = :id");
+		query.setParameter("id", id);
+		query.executeUpdate();
+
+		session.getTransaction().commit();
+		factory.close();
+	}
+	
 }
