a4727504129fef48b91eca3cb3899c334ce53cef
Tracy Cummings <TLCummings90@gmail.com>
2019-03-21 09:37:34 -0400

    added isPublic to Game (added getter/setter, toString and hashcode stuff), added @service annotation to GameService

--- src/main/java/com/revature/services/GameService.java
+++ src/main/java/com/revature/services/GameService.java
@@ -7,10 +7,12 @@ import javax.persistence.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.springframework.stereotype.Service;
 
 import com.revature.models.Game;
 import com.revature.models.User;
 
+@Service
 public class GameService {
 	
 	public GameService() {
