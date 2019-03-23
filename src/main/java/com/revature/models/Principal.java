4128d0808945356977d6ef93b1dcfecc13b34f0e
Tracy Cummings <TLCummings90@gmail.com>
2019-03-23 14:03:53 -0400

    added Jwt and filter with principal object

---
+++ src/main/java/com/revature/models/Principal.java
@@ -0,0 +1,84 @@
+package com.revature.models;
+
+public class Principal {
+
+	private int id;
+	private String password;
+	private String username;
+	
+	
+	public Principal() {
+		super();
+	}
+	
+	public Principal(int id, String password, String username) {
+		super();
+		this.id = id;
+		this.password = password;
+		this.username = username;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Principal other = (Principal) obj;
+		if (id != other.id)
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Principal [id=" + id + ", password=" + password + ", username=" + username + "]";
+	}
+	
+}
---
+++ src/main/java/com/revature/util/JwtGenerator.java
@@ -0,0 +1,54 @@
+package com.revature.util;
+
+import java.util.Date;
+
+import org.apache.log4j.Logger;
+
+import com.revature.models.User;
+
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+/**
+ * 
+ * @author Team Cirrus
+ *
+ */
+public class JwtGenerator {
+	
+	private static Logger log = Logger.getLogger(JwtGenerator.class);
+	
+	/**
+	 * 
+	 * @param subject
+	 * @return	a web token for the user with their detail object provided as parameter.
+	 */
+	public static String createJwt(User subject) {
+		log.info("Creating new JWT for: " + subject.getUsername());
+		
+		// The JWT Signature Algorithm used to sign the token
+		SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+		
+		long nowMillis = System.currentTimeMillis();
+		
+		// Configure the JWT and set its claims
+		JwtBuilder builder = Jwts.builder()
+				.setId(Integer.toString(subject.getUserId()))
+				.setSubject(subject.getUsername())
+				.setIssuer("project2")
+				.claim("password", subject.getPassword())
+				.setExpiration(new Date(nowMillis + JwtConfig.EXPIRATION * 1000))
+				.signWith(signatureAlgorithm, JwtConfig.signingKey);
+		
+		log.info("JWT successfully created");
+		
+		// Build the JWT and serialize it into a compact, URL-safe string
+		return builder.compact();
+	}
+	
+	private JwtGenerator() {
+		super();
+	}
+
+}
